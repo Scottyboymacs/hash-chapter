a0d423988fc87f6f11daca36798b9531d0e467f9
Scott McKeon <scott.mckeon@chicagobooth.edu>
2022-04-15 13:27:27 +0000

    Almsot done

--- hash_find_value.rb
+++ hash_find_value.rb
@@ -7,3 +7,10 @@
 sample_hash = {:a => 100, :b => 200, :c => 300, :d => 400, :e => rand(200), :f => 600, :g => 0 }
 
 p "Enter an integer to find:"
+value = gets.chomp.to_i
+
+if sample_hash.key(value) == nil
+	p "Could not find the integer " + value.to_s
+else
+	p value.to_s + " is under the key: " + sample_hash.key(value).to_s
+end
--- hash_person.rb
+++ hash_person.rb
@@ -1,2 +1,6 @@
 # Ask for a name, age, and occupation; store the values in a Hash; display the Hash.
 p "Enter a name, age, and occupation separated by spaces:"
+person = gets.chomp.split
+hashy = { :name => person.at(0), :age => person.at(1).to_i, :occupation => person.at(2) }
+
+p hashy
